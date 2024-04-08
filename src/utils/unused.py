af9e9fae50f62ce272acdc9ef29f004f682cd2b2
Ishan Nangia <ishannangia.123@gmail.com>
2024-04-08 15:08:11 +0530

    create unused functions file

---
+++ src/utils/unused.py
@@ -0,0 +1,27 @@
+from .general_utils import open_yaml_file, save_yaml_file
+
+
+def check_ultralytics_settings(address):
+    """
+    Use it to check if original settings.yaml file has datasets_dir pointing to the current
+    dir or not.
+
+    :Usage:
+    ultralytics_settings_address = "/root/.config/Ultralytics/settings.yaml"
+    result = check_ultralytics_settings(ultralytics_settings_address)
+
+    :param address:
+    :return:
+    """
+    if address == "":
+        print("No address for Ultralytics Settings given...")
+        return False
+    # checking if config is problematic or not
+    settings = open_yaml_file(address)
+    if settings['datasets_dir'] != ".":
+        settings['datasets_dir'] = "."
+        save_yaml_file(settings, address)
+        print('Changed Datasets Directory in Ultralytics Settings. Rerun please...')
+        return False
+    else:
+        return True
